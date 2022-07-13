19e71e14383aadd8f51ceba2cebfbbe6d130f79f
Derek YIP <derekyip@esdlife.com>
2022-07-13 18:20:48 +0800

    arrow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,20 @@ function Hero() {
     <div>
         <div className="hero_wrapper">
             <div className="hero">
-                <div className="hero_left">
-                    <img src="https://picsum.photos/id/684/1200/1600" alt=""/>
+                <div className="hero_slider">
+
                 </div>
-                <div className="hero_right column_center_center">
+                <div className="arrow_wrapper">
+                    <div className="arrow_left"></div> 
+                    <div className="arrow_right"></div>
+                </div>
+                <div className="hero_inner column_center_center">
+                    <div className="content">
+                        <h1>Marry With Promises</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto perspiciatis debitis necessitatibus! Debitis illo est quam perspiciatis placeat aliquam ut recusandae, maxime hic adipisci veniam dolores rem. Debitis impedit odit, quibusdam alias molestiae eius sed non doloremque laborum animi quae? Error necessitatibus temporibus, similique nisi qui impedit quas quibusdam omnis voluptatum ullam. Natus labore a illo autem fugiat recusandae voluptatibus officiis, possimus excepturi similique cum vel dolores atque ad quo commodi, ex laborum numquam sint? Aliquam, cupiditate id. Assumenda animi, tempore quisquam quam deserunt ducimus, a minus nam similique facere eligendi iure iusto distinctio asperiores necessitatibus, illum quaerat at eum.</p>
+                    </div>
+                </div>
+                <div className="hero_inner column_center_center">
                     <div className="content">
                         <h1>Marry With Promises</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto perspiciatis debitis necessitatibus! Debitis illo est quam perspiciatis placeat aliquam ut recusandae, maxime hic adipisci veniam dolores rem. Debitis impedit odit, quibusdam alias molestiae eius sed non doloremque laborum animi quae? Error necessitatibus temporibus, similique nisi qui impedit quas quibusdam omnis voluptatum ullam. Natus labore a illo autem fugiat recusandae voluptatibus officiis, possimus excepturi similique cum vel dolores atque ad quo commodi, ex laborum numquam sint? Aliquam, cupiditate id. Assumenda animi, tempore quisquam quam deserunt ducimus, a minus nam similique facere eligendi iure iusto distinctio asperiores necessitatibus, illum quaerat at eum.</p>

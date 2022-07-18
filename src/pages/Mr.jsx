58809ae1e80802cf6625816f330ff52656f8e29e
Your Name <you@example.com>
2022-07-19 00:07:57 +0800

    git

--- src/pages/Mr.jsx
+++ src/pages/Mr.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import '../scss/Mr.scss'
 import Button from '../components/Button'
 import Parallax from '../components/Parallax'
 import '../scss/Poster.scss'
 
+
 function Mr() {
   return (
-    <div className='mr_page'>
+    <div>
       <div className="layer">
         <Parallax height="100vh" background_img="https://images.pexels.com/photos/753994/pexels-photo-753994.jpeg"/>
-        <div className="cube">
-          <Button height="200px" width="200px" background_img="https://picsum.photos/700/700" />
+        <div className="cube_wrapper center_center">
+          <div className="cube">
+            <Button height="200px" width="200px" background_img="https://picsum.photos/700/700" />
+          </div>
         </div>
       </div>
     </div>
